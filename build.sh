41ac740a9be52924ed508d4a9ef7a8191b86d6cd
irala <nineirm@gmail.com>
2021-06-12 13:20:19 +0200

    changes

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-docker run --rm -p 8081:8081 -d identity
+#!/bin/bash
+
+BUILDALL=""
+CORES=$(nproc --all)
+
+if [[ -z $1 ]]; then
+    BUILDALL="true"
+fi
+
+if [[ $1 == "identity" ]] || [[ -n $BUILDALL ]]; then
+    #building identity
+    docker build --rm -f identity/dockerfile -t identity identity
+fi
+# docker run --rm -p 8081:8081 -d identity
