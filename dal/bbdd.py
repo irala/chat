06f1bb1cba74002ea3817eefe1f094a03d4c4c7e
Nuria <nineirm@gmail.com>
2021-07-10 18:31:12 +0200

    changes in dal

--- dal/bbdd.py
+++ dal/bbdd.py
@@ -17,6 +17,19 @@ class bbdd():
 
         print("Conexion bien")
 
+
+    def getAll(self):
+        try:
+            self.cursor.execute("SELECT * FROM user_account")
+            users=self.cursor.fetchall()
+            self.connection.commit()
+
+            for user in users:
+                print("username "+ user[1]+ " password "+user[2])
+
+        except Exception as e:
+            raise
+
     def readUser(self, id):
         sql = 'SELECT id,username, password FROM user_account WHERE ID ={}'.format(
             id)
@@ -24,6 +37,7 @@ class bbdd():
         try:
             self.cursor.execute(sql)
             user = self.cursor.fetchone()
+            self.connection.commit()
 
             print("id:", user[0])
             print("username:", user[1])
@@ -33,13 +47,30 @@ class bbdd():
             raise
 
 	
-    def createUser(self, username, password):	
+    def createUser(self,id,username, password):	
         try:
-			self.cursor.execute('INSERT INTO user_account VALUES(%s, %s)',(username, password) )
-			self.connection.commit()
 
-			print('new user inserted')
+            with self.connection.cursor() as cur:
 
+                cur.execute('INSERT INTO user_account VALUES(%s, %s, %s)', 
+                (id,username,password)) 
+                self.connection.commit()
+
+                print('new user inserted')
+
+        except Exception as e:
+            print('error'+ str(e))
+
+    
+    def deleteUser(self, username):
+        try:
+            sql="DELETE FROM user_account WHERE USERNAME = '"+ username + "'"
+            print(sql)
+
+            self.cursor.execute(sql)
+            self.connection.commit()
+            
+            print('delete user: '+ username)
         except Exception as e:
             raise
 
@@ -48,4 +79,7 @@ class bbdd():
 
 
 database = bbdd()
-database.createUser("pepito", "hola")
+database.getAll()
+#database.readUser(1)
+#database.createUser(1,'prueba', '1234')
+#database.deleteUser('pepito')
