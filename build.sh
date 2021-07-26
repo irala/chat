3daf932be1a9fc7813eca91297306e459e33f87b
Nuria <nineirm@gmail.com>
2021-07-26 11:12:39 +0200

    changes in docker compose

--- build.sh
+++ build.sh
@@ -11,4 +11,10 @@ if [[ $1 == "identity" ]] || [[ -n $BUILDALL ]]; then
     #building identity
     docker build --rm -f identity/dockerfile -t identity identity
 fi
+
+
+if [[ $1 == "chatting" ]] || [[ -n $BUILDALL ]]; then
+    #building identity
+    docker build --rm -f chatting/dockerfile -t chatting chatting
+fi
 # docker run --rm -p 8081:8081 -d identity
